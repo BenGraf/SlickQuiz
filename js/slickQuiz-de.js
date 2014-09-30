0f6992cde9771044acd104e892031b3d20659f7d
BenGraf <BenGraf@users.noreply.github.com>
2014-09-30 17:48:51 +0200

    Add German translation of default text.

---
+++ js/slickQuiz-de.js
@@ -0,0 +1,14 @@
+// German translations for default captions
+$(function () {
+    $('#slickQuiz').slickQuiz({
+        checkAnswerText: 'Antwort &uuml;berpr&uuml;fen.',
+        nextQuestionText: 'N&auml;chste Frage &raquo;',
+        backButtonText: '',
+        tryAgainText: '',
+        questionCountText: 'Frage %current von %total',
+        preventUnansweredText: 'Sie m&uml;ssen mindestens eine Antwort ausw&auml;hlen.',
+        questionTemplateText: '%count. %text',
+        scoreTemplateText: '<strong>Ergebnis:</strong> %score von %total Fragen.',
+        nameTemplateText: '<span>Quiz: </span>%name'
+    });
+});
